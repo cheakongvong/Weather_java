0223cf9a7c9538af34099d63cf6f89708b402e8b
kimly <chhaykimly7@gmail.com>
2023-12-07 21:35:06 +0700

    Do more Activities and connect with Adapters

---
+++ app/src/main/java/com/example/weather_java/Activity/SunActivity.java
@@ -0,0 +1,16 @@
+package com.example.weather_java.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.weather_java.R;
+
+public class SunActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sun);
+    }
+}
